68f996ecfa78a6371ca6f87c51578a0760f054d7
Ishan13 <shrestha.ishan2@gmail.com>
2024-03-05 18:46:52 -0500

    using format function

---
+++ FormatMethod.py
@@ -0,0 +1,8 @@
+name = "John Doe"
+age = 21
+# We can use format() function to format strings by replacing placeholders with values, we use {} as placeholders.
+message = "Hello, my name is {} and I am {} years old.".format(name, age)
+print(message)
+
+
+
