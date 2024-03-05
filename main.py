8cc6ae9c7d9a0be19e25cd43be8b72c425ea1124
Ishan13 <shrestha.ishan2@gmail.com>
2024-03-05 18:04:17 -0500

    Strings

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-a = 5
-x = 10.76
-s = "Hello World"
+my_string = "Hello, World!"
 
-print(type(a))
-a = 100.68
-print(type(a))
-print(type(x))
-print(type(s))
+# String Indexing, it's used to access individual characters. Using []
+print(my_string[0])
+print(my_string[7])
+
+# String Slicing allows us to access a range of characters in a string. syntax [start:stop:step]
+print(my_string[0:5])
+print(my_string[7:])
+print(my_string[:5])
+print(my_string[::2])  # it steps every second character. we can call this step or stride
+
+# We use len() method to find out the length of the string.
+print(len(my_string))
+
