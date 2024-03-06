d2e45350223c1aaa7b2297ea587450e549025bf6
Ishan13 <shrestha.ishan2@gmail.com>
2024-03-06 13:40:26 -0500

    format each statement to be centered in a field of 40 characters using f-strings and concatenate them with a seperator.

---
+++ FirstChallenge.py
@@ -0,0 +1,8 @@
+statement1 = "I fly in an invisible airplane"
+statement2 = "My name is Wonder Woman"
+
+# format each statement to be centered in a field of 40 characters.
+f_stat1 = f"{statement1.center(40)}"
+f_stat2 = f"{statement2.center(40)}"
+
+print(f_stat1 + "===>" + f_stat2)
