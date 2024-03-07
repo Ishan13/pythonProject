4b24fc6551ca497d46be9968d54a9121721d5d66
Ishan13 <shrestha.ishan2@gmail.com>
2024-03-06 21:34:28 -0500

    New Practice ifelse

--- Practice.py
+++ Practice.py
@@ -4,9 +4,32 @@
 # print("The perimeter of the rectangle is : " f'{perimeter}')
 
 
-pi = 3.14
-radius = int(input("Enter the radius of the circle:"))
-area = pi * radius ** 2
-print("The area of the circle is", area)
+# pi = 3.14
+# radius = int(input("Enter the radius of the circle:"))
+# area = pi * radius ** 2
+# print("The area of the circle is", area)
 
+# number = int(input("Enter a positive number:"))
+# odd_even = number % 2
+# if odd_even == 0:
+#     print(f"Number {number} is Even.")
+# else:
+#     print(f"Number {number} is Odd.")
+
+grade = float(input("Enter the grade of the student: "))
+
+if grade >= 90:
+    print("Outstanding")
+elif grade >= 80:
+    print("Excellent")
+elif grade >= 70:
+    print("Very Good")
+elif grade >= 60:
+    print("Good")
+elif grade >= 50:
+    print("Fair")
+elif grade >= 40:
+    print("Below Average")
+else:
+    print("Failed/Absent")
 
