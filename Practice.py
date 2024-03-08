afe5d577767e4f3d163550206bc3e2f5d1394b66
Ishan13 <shrestha.ishan2@gmail.com>
2024-03-08 10:52:48 -0500

    function practice

--- Practice.py
+++ Practice.py
@@ -16,20 +16,50 @@
 # else:
 #     print(f"Number {number} is Odd.")
 
-grade = float(input("Enter the grade of the student: "))
+# grade = float(input("Enter the grade of the student: "))
+#
+# if grade >= 90:
+#     print("Outstanding")
+# elif grade >= 80:
+#     print("Excellent")
+# elif grade >= 70:
+#     print("Very Good")
+# elif grade >= 60:
+#     print("Good")
+# elif grade >= 50:
+#     print("Fair")
+# elif grade >= 40:
+#     print("Below Average")
+# else:
+#     print("Failed/Absent")
 
-if grade >= 90:
-    print("Outstanding")
-elif grade >= 80:
-    print("Excellent")
-elif grade >= 70:
-    print("Very Good")
-elif grade >= 60:
-    print("Good")
-elif grade >= 50:
-    print("Fair")
-elif grade >= 40:
-    print("Below Average")
-else:
-    print("Failed/Absent")
+# sample_dict = {1: 'Coffee', 2: 'Tea', 3: 'Juice'}
+# for x in sample_dict:
+#     print(x)
 
+# def recursion(num):
+#     print(num)
+#     next = num - 3
+#     if next > 1:
+#         recursion(next)
+#
+# recursion(11)
+
+# num = 9
+# class Car:
+#     num = 5
+#     bathrooms = 2
+#
+# def cost_evaluation(num):
+#     num = 10
+#     return num
+#
+# class Bike():
+#     num = 11
+#
+# cost_evaluation(num)
+# car = Car()
+# bike = Bike()
+# car.num = 7
+# Car.num = 2
+# print(num)
