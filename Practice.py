4d8b0ee1b782cc837d8bcc8c370b17e76cfca288
Ishan13 <shrestha.ishan2@gmail.com>
2024-03-11 21:47:23 -0400

    loop and nested loop practice

--- Practice.py
+++ Practice.py
@@ -63,3 +63,68 @@
 # car.num = 7
 # Car.num = 2
 # print(num)
+
+# for i in range(25):
+#     x = (i+1)*2
+#     print(x)
+
+# x = 0
+# for i in range(1, 6):
+#     x += i
+#     print(x)
+#
+# print(f"The running summation is {x}")
+
+# x = 1
+# for i in range(1, 6):
+#     x = x * i
+#
+# print(x)
+
+# start = int(input("Enter the starting number of the range: "))
+# end = int(input("Enter the ending number of the range: "))
+# sum = 0
+# for num in range(start, end+1):
+#     sum += num
+#
+# print(sum)
+
+# number = int(input("Enter a number: "))
+#
+# if number > 1:
+#     for i in range(2, int(number**0.5)+1):
+#         if (number % i) == 0:
+#             print(f"{number} is not a prime number.")
+#             break
+#     else:
+#         print(f"{number} is a prime number.")
+# else:
+#     print(f"{number} is not a prime number.")
+
+# res = 0
+# n = int(input("Enter the number: "))
+# for i in range(1, n):
+#     if n % i == 0:
+#         res = res + i
+#
+# if res == n:
+#     print(f"{n} is a perfect number")
+# else:
+#     print(f"{n} is not a perfect number")
+
+# n = int(input("Enter a number: "))
+# n2 = int(input("Enter second number: "))
+# mul = 0
+# for i in range(n, n2+1):
+#     for j in range(1, 11):
+#         mul = j * i
+#         print(f"{i} x {j}  = {mul}")
+#
+#     print("=============================")
+
+# for i in range(1, 10):
+#     if i % 3 == 0:
+#         print(i)
+#     else:
+#         print(i, end=" ")
+
